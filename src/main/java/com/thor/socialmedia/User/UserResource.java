3aa01747ac85e8e1ed59bc45d345a1ecf4f7300c
thorcamara <thorcamaraalthoff@gmail.com>
2023-08-01 17:15:54 -0300

    Implemented DELETE Method to delete a User Resource

--- src/main/java/com/thor/socialmedia/User/UserResource.java
+++ src/main/java/com/thor/socialmedia/User/UserResource.java
@@ -32,6 +32,11 @@ public class UserResource {
         return user;
     }
 
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        service.deleteById(id);
+    }
+
     @PostMapping("/users")
     public ResponseEntity<User> createUser(@RequestBody User user) {
 
