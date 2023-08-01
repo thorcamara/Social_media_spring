46c9e6adacadecdc30e89b8bb5824de2b7938b15
thorcamara <thorcamaraalthoff@gmail.com>
2023-08-01 16:34:23 -0300

    Enchanced POST Method to return correct HTTP Status Code and Location

--- src/main/java/com/thor/socialmedia/User/UserResource.java
+++ src/main/java/com/thor/socialmedia/User/UserResource.java
@@ -1,7 +1,10 @@
 package com.thor.socialmedia.User;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -13,21 +16,26 @@ public class UserResource {
         this.service = service;
     }
 
-    //GET /users
     @GetMapping("/users")
     public List<User> retrieveAllUsers(){
         return service.findAll();
     }
 
-    //GET /users
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id){
         return service.findOne(id);
     }
 
-    //POST /users
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
-        service.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
