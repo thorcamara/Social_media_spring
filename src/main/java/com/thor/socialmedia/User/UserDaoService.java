e1f887addfeaf138e93278bacb8520028b18ba4a
thorcamara <thorcamaraalthoff@gmail.com>
2023-08-01 15:42:27 -0300

    Created User Bean and UserDaoService

---
+++ src/main/java/com/thor/socialmedia/User/UserDaoService.java
@@ -0,0 +1,27 @@
+package com.thor.socialmedia.User;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+    // JPA/Hibernate > Database
+    // UserDaoService > Static Lists
+
+    private static List<User> users = new ArrayList<>();
+
+    static {
+        users.add(new User(1,"Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(2,"Eve", LocalDate.now().minusYears(25)));
+        users.add(new User(3,"Jim", LocalDate.now().minusYears(20)));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+    //public User save(User user) {
+    //public User findOne(int id) {
+}
