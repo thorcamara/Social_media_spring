bdbb4fa90fb4370d1bf58ffc88ce28af9e0e1465
thorcamara <thorcamaraalthoff@gmail.com>
2023-08-01 17:35:39 -0300

    Implemented Validations for REST API

--- src/main/java/com/thor/socialmedia/User/UserResource.java
+++ src/main/java/com/thor/socialmedia/User/UserResource.java
@@ -1,5 +1,6 @@
 package com.thor.socialmedia.User;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -38,7 +39,7 @@ public class UserResource {
     }
 
     @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
 
         User savedUser = service.save(user);
 
