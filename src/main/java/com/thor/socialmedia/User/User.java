719dac8329ef5d9c7219a0d22092dba314495157
thorcamara <thorcamaraalthoff@gmail.com>
2023-08-02 16:09:51 -0300

    Created Post Entity with Many to One Relationship with User Entity

--- src/main/java/com/thor/socialmedia/User/User.java
+++ src/main/java/com/thor/socialmedia/User/User.java
@@ -1,13 +1,16 @@
 package com.thor.socialmedia.User;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity(name = "user_details")
 public class User {
@@ -27,6 +30,10 @@ public class User {
     @Past(message = "Birth Date should be in the past")
     //@JsonProperty("birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Post> posts;
 
 
     public User(Integer id, String name, LocalDate birthDate) {
