cfe8a4eb7203488713446208176cd4c9e1b1795b
thorcamara <thorcamaraalthoff@gmail.com>
2023-08-02 14:49:23 -0300

    Created User Entity and some test data (H2)

--- src/main/java/com/thor/socialmedia/User/User.java
+++ src/main/java/com/thor/socialmedia/User/User.java
@@ -1,13 +1,19 @@
 package com.thor.socialmedia.User;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name = "user_details")
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message="Name should have atleast 2 characters")
