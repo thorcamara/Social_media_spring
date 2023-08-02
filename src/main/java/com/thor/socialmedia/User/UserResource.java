652762adccc99895adc2a860d8e82982aeca4caf
thorcamara <thorcamaraalthoff@gmail.com>
2023-08-02 14:06:55 -0300

    Implemented HATEOAS for REST API

--- src/main/java/com/thor/socialmedia/User/UserResource.java
+++ src/main/java/com/thor/socialmedia/User/UserResource.java
@@ -1,6 +1,10 @@
 package com.thor.socialmedia.User;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -23,14 +27,24 @@ public class UserResource {
         return service.findAll();
     }
 
+    //http://localhost:8080/users
+
+    //EntityModel
+    //WebMvcLinkBuilder
+
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public EntityModel<User> retrieveUser(@PathVariable int id){
         User user = service.findOne(id);
 
         if(user==null)
             throw new UserNotFoundException("id:"+id);
 
-        return user;
+        EntityModel<User> entityModel = EntityModel.of(user);
+
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(link.withRel("all-users"));
+
+        return entityModel;
     }
 
     @DeleteMapping("/users/{id}")
